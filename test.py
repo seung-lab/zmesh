a2d18bae811d4aa4f9fac06b127d79c24c466745
William Silversmith <william.silversmith@gmail.com>
2019-04-30 21:57:22 -0400

    feat: add simple manual cube generation test for obj and ply

---
+++ test.py
@@ -0,0 +1,18 @@
+import numpy as np
+import zmesh
+
+labels = np.zeros( (50, 50, 50), dtype=np.uint32)
+labels[1:-1, 1:-1, 1:-1] = 1
+
+mesher = zmesh.Mesher( (1,1,1) )
+mesher.mesh(labels)
+mesh = mesher.get_mesh(1, normals=False, simplification_factor=0, max_simplification_error=100)
+
+print(mesh)
+
+with open('wow.obj', 'bw') as f:
+  f.write(mesh.to_ply())
+
+with open('wow.ply', 'bw') as f:
+  f.write(mesh.to_ply())
+
