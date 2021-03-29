b939f9245092ad9ddefa9e20cb36f7f66e79b4fb
Norman Rzepka <code@normanrz.com>
2021-03-29 17:32:56 +0200

    unformat

--- cMesher.hpp
+++ cMesher.hpp
@@ -7,32 +7,29 @@
 #include <zi/mesh/quadratic_simplifier.hpp>
 #include <zi/vl/vec.hpp>
 
-struct MeshObject
-{
+struct MeshObject {
   std::vector<float> points;
   std::vector<float> normals;
   std::vector<unsigned int> faces;
 };
 
 template <typename PositionType, typename LabelType, typename SimplifierType>
-class CMesher
-{
-private:
+class CMesher {
+ private:
   zi::mesh::marching_cubes<PositionType, LabelType> marchingcubes_;
   zi::mesh::simplifier<SimplifierType> simplifier_;
   std::vector<double> voxelresolution_;
 
-public:
-  CMesher(const std::vector<double> &voxelresolution)
-  {
+ public:
+  CMesher(const std::vector<double> &voxelresolution) {
     voxelresolution_ = voxelresolution;
   }
-  ~CMesher(){};
+  ~CMesher() {};
 
   void mesh(
-      const std::vector<LabelType> &data,
-      unsigned int sx, unsigned int sy, unsigned int sz)
-  {
+      const std::vector<LabelType> &data, 
+      unsigned int sx, unsigned int sy, unsigned int sz
+    ) {
     // Create Marching Cubes class for type T volume
 
     const LabelType *a = &data[0];
@@ -40,103 +37,96 @@ public:
     marchingcubes_.marche(a, sx, sy, sz);
   }
 
-  std::vector<LabelType> ids()
-  {
+  std::vector<LabelType> ids() {
     std::vector<LabelType> keys;
     for (auto it = marchingcubes_.meshes().begin();
-         it != marchingcubes_.meshes().end(); ++it)
-    {
+         it != marchingcubes_.meshes().end(); ++it) {
       keys.push_back(it->first);
     }
 
     return keys;
   }
 
-  PositionType pack_coords(PositionType x, PositionType y, PositionType z)
-  {
-    return marchingcubes_.pack_coords(x, y, z);
+  PositionType pack_coords(PositionType x, PositionType y, PositionType z) {
+    return marchingcubes_.pack_coords(x,y,z);
   }
 
   MeshObject get_mesh(
       LabelType segid, bool generate_normals,
       int simplification_factor,
-      int max_simplification_error)
-  {
+      int max_simplification_error
+    ) {
 
     // MC produces no triangles if either
     // none or all voxels were labeled.
     MeshObject empty_obj;
-    if (marchingcubes_.count(segid) == 0)
-    {
+    if (marchingcubes_.count(segid) == 0) { 
       return empty_obj;
     }
 
-    std::vector<zi::vl::vec<PositionType, 3>> triangles = marchingcubes_.get_triangles(segid);
+    std::vector< zi::vl::vec< PositionType, 3> > triangles = marchingcubes_.get_triangles(segid);
 
     return simplify(
-        triangles,
-        generate_normals,
-        simplification_factor,
-        max_simplification_error);
+      triangles,
+      generate_normals,
+      simplification_factor,
+      max_simplification_error
+    );
   }
 
-  MeshObject simplify(
-      const std::vector<zi::vl::vec<PositionType, 3>> &triangles,
+  MeshObject simplify(      
+      const std::vector< zi::vl::vec< PositionType, 3> >& triangles,
       bool generate_normals,
       int simplification_factor,
-      int max_simplification_error)
-  {
+      int max_simplification_error
+    ) {
 
     MeshObject obj;
 
     zi::mesh::int_mesh<PositionType, LabelType> im;
     im.add(triangles);
     im.template fill_simplifier<SimplifierType>(
-        simplifier_,
-        0, 0, 0,
-        voxelresolution_[2], voxelresolution_[1], voxelresolution_[0]);
+      simplifier_, 
+      0, 0, 0, 
+      voxelresolution_[2], voxelresolution_[1], voxelresolution_[0]
+    );
     simplifier_.prepare(generate_normals);
 
-    if (simplification_factor > 0)
-    {
+    if (simplification_factor > 0) {
       // This is the most cpu intensive line
       simplifier_.optimize(
           simplifier_.face_count() / simplification_factor,
-          max_simplification_error);
+          max_simplification_error
+      );
     }
 
-    std::vector<zi::vl::vec<SimplifierType, 3>> points;
-    std::vector<zi::vl::vec<SimplifierType, 3>> normals;
-    std::vector<zi::vl::vec<unsigned, 3>> faces;
+    std::vector<zi::vl::vec<SimplifierType, 3> > points;
+    std::vector<zi::vl::vec<SimplifierType, 3> > normals;
+    std::vector<zi::vl::vec<unsigned, 3> > faces;
 
     simplifier_.get_faces(points, normals, faces);
     obj.points.reserve(3 * points.size());
     obj.faces.reserve(3 * faces.size());
 
-    if (generate_normals)
-    {
+    if (generate_normals) {
       obj.normals.reserve(3 * points.size());
     }
 
-    for (auto v = points.begin(); v != points.end(); ++v)
-    {
+    for (auto v = points.begin(); v != points.end(); ++v) {
       obj.points.push_back((*v)[2]);
       obj.points.push_back((*v)[1]);
       obj.points.push_back((*v)[0]);
     }
 
-    if (generate_normals)
-    {
-      for (auto vn = normals.begin(); vn != normals.end(); ++vn)
-      {
+    if (generate_normals) {
+      for (auto vn = normals.begin(); vn != normals.end(); ++vn) {
         obj.normals.push_back((*vn)[2]);
         obj.normals.push_back((*vn)[1]);
         obj.normals.push_back((*vn)[0]);
       }
     }
 
-    for (auto f = faces.begin(); f != faces.end(); ++f)
-    {
+    for (auto f = faces.begin(); f != faces.end(); ++f) {
       obj.faces.push_back((*f)[0]);
       obj.faces.push_back((*f)[2]);
       obj.faces.push_back((*f)[1]);
@@ -146,42 +136,43 @@ public:
   }
 
   MeshObject simplify_points(
-      const uint64_t *points,
-      const size_t Nv,
-      bool generate_normals,
-      int simplification_factor,
-      int max_simplification_error)
-  {
+    const uint64_t* points,
+    const size_t Nv,
+    bool generate_normals,
+    int simplification_factor,
+    int max_simplification_error
+  ) {
 
-    std::vector<zi::vl::vec<PositionType, 3>> triangles;
+    std::vector< zi::vl::vec< PositionType, 3> > triangles;
     triangles.reserve(Nv);
 
-    for (size_t i = 0; i < Nv; i++)
-    {
+    for (size_t i = 0; i < Nv; i++) {
       triangles.push_back(
-          zi::vl::vec<PositionType, 3>(
-              points[3 * i + 0],
-              points[3 * i + 1],
-              points[3 * i + 2]));
+        zi::vl::vec<PositionType, 3>(
+          points[3 * i + 0], 
+          points[3 * i + 1], 
+          points[3 * i + 2]
+        )
+      );
     }
 
     return simplify(
-        triangles,
-        generate_normals,
-        simplification_factor,
-        max_simplification_error);
+      triangles, 
+      generate_normals, 
+      simplification_factor, 
+      max_simplification_error
+    );
   }
 
-  void clear()
-  {
+  void clear() {
     marchingcubes_.clear();
     simplifier_.clear();
   }
 
-  bool erase(LabelType segid)
-  {
+  bool erase(LabelType segid) {
     return marchingcubes_.erase(segid);
   }
 };
+
 
 #endif
