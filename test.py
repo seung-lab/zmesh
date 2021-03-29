784aeff96de52311d0fbf4468d0d0f62c86f3326
Norman Rzepka <code@normanrz.com>
2021-03-29 17:44:20 +0200

    adds test from nkemnitz

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import zmesh
 labels = np.zeros( (50, 50, 50), dtype=np.uint32)
 labels[1:-1, 1:-1, 1:-1] = 1
 
-mesher = zmesh.Mesher( (1,1,1) )
+mesher = zmesh.Mesher( (3.141,1,1) )
 mesher.mesh(labels)
-mesh = mesher.get_mesh(1, normals=False, simplification_factor=0, max_simplification_error=100)
+mesh = mesher.get_mesh(1, normals=False, simplification_factor=100, max_simplification_error=100)
 
 print(mesh)
 
