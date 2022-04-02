80f00c58492726a3c687a23b8b91c1b1845f8c52
William Silversmith <william.silversmith@gmail.com>
2022-04-02 00:46:31 -0400

    test: add scikit image in very rough comparison to zmesh

--- perf.py
+++ perf.py
@@ -7,31 +7,79 @@ import zmesh
 import time
 
 def result(label, dt, data, N):
-	voxels = data.size
-	mvx = voxels // (10 ** 6)
-	print(f"{label}: {dt:02.3f}s, {N * mvx / dt:.2f} MVx/sec, N={N}")
+    voxels = data.size
+    mvx = voxels // (10 ** 6)
+    print(f"{label}: {dt:02.3f}s, {N * mvx / dt:.2f} MVx/sec, N={N}")
 
-def test_marching_cubes():
-	labels = np.load("./connectomics.npy")
-	labels = np.ascontiguousarray(labels)
-	mesher = zmesh.Mesher((1,1,1))
+def test_zmesh_marching_cubes():
+    labels = np.zeros((512,512,512), dtype=np.uint8, order="C")
+    mesher = zmesh.Mesher((1,1,1))
+    N = 1
+    start = time.time()
+    for _ in range(N):
+        mesher.mesh(labels)
+    end = time.time()
+    result("marching cubes (blank)", end - start, labels, N=N)
 
-	N = 1
-	start = time.time()
-	for _ in range(N):
-		mesher.mesh(labels)
-	end = time.time()
-	result("marching cubes (connectomics.npy)", end - start, labels, N=N)
+    labels = np.ones((512,512,512), dtype=np.uint8, order="C")
+    mesher = zmesh.Mesher((1,1,1))
+    N = 1
+    start = time.time()
+    for _ in range(N):
+        mesher.mesh(labels, close=True)
+    end = time.time()
+    result("marching cubes (filled)", end - start, labels, N=N)
 
-	labels = np.random.randint(0,1000, size=(448,448,448), dtype=np.uint32)
-	labels = np.ascontiguousarray(labels)
-	mesher = zmesh.Mesher((1,1,1))
+    labels = np.load("./connectomics.npy")
+    labels = np.ascontiguousarray(labels)
+    mesher = zmesh.Mesher((1,1,1))
 
-	N = 1
-	start = time.time()
-	for _ in range(N):
-		mesher.mesh(labels)
-	end = time.time()
-	result("marching cubes (random)", end - start, labels, N=N)
+    N = 1
+    start = time.time()
+    for _ in range(N):
+        mesher.mesh(labels)
+    end = time.time()
+    result("marching cubes (connectomics.npy)", end - start, labels, N=N)
 
-test_marching_cubes()
+    labels = np.random.randint(0,1000, size=(448,448,448), dtype=np.uint32)
+    labels = np.ascontiguousarray(labels)
+    mesher = zmesh.Mesher((1,1,1))
+
+    N = 1
+    start = time.time()
+    for _ in range(N):
+        mesher.mesh(labels)
+    end = time.time()
+    result("marching cubes (random)", end - start, labels, N=N)
+
+def test_scikit_marching_cubes():
+    import skimage.measure
+
+    print("marching cubes (blank) NOT HANDLED")
+    print("marching cubes (filled) NOT HANDLED")
+    
+    labels = np.ones((512,512,512))
+    labels = np.load("./connectomics.npy")
+    labels = np.ascontiguousarray(labels)
+
+    N = 1
+    start = time.time()
+    for _ in range(N):
+        skimage.measure.marching_cubes(labels)
+    end = time.time()
+    result("marching cubes (connectomics.npy)", end - start, labels, N=N)
+
+    labels = np.random.randint(0,1000, size=(448,448,448), dtype=np.uint32)
+    labels = np.ascontiguousarray(labels)
+
+    N = 1
+    start = time.time()
+    for _ in range(N):
+        skimage.measure.marching_cubes(labels)
+    end = time.time()
+    result("marching cubes (random)", end - start, labels, N=N)
+
+# print("ZMESH")
+# test_zmesh_marching_cubes()
+print("SKIMAGE")
+test_scikit_marching_cubes()
