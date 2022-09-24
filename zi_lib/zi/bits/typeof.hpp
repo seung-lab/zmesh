25c39c236694f115d5321d11bbd194be0613d7fd
William Silversmith <william.silversmith@gmail.com>
2022-09-24 12:03:33 -0400

    refactor: remove alternative boost defns of typeof

--- zi_lib/zi/bits/typeof.hpp
+++ zi_lib/zi/bits/typeof.hpp
@@ -20,28 +20,9 @@
 #define ZI_BITS_TYPEOF_HPP 1
 
 #include <zi/config/config.hpp>
-#
-#if defined( __GXX_EXPERIMENTAL_CXX0X__ )
+
 #  ifndef __typeof__
 #    define __typeof__( expr ) decltype( expr )
 #  endif
-#elif defined( ZI_CXX_GCC )
-#  ifndef __typeof__
-#    define __typeof__( expr ) typeof( expr )
-#  endif
-#
-#elif defined( ZI_CXX_MSVC )
-#  ifndef ZI_NO_BOOST
-#    include <boost/typeof/typeof.hpp>
-#    define __typeof__( expr ) BOOST_TYPEOF( expr )
-#  else
-#    error "no typeof implementation available"
-#  endif
-#
-#else
-#  ifndef __typeof__
-#    error "no typeof implementation available"
-#  endif
-#endif
-#
+
 #endif
