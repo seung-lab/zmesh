1037747f9aac476eb1ae32343188ec4078eff7d1
William Silversmith <william.silversmith@gmail.com>
2024-10-25 17:18:19 -0400

    refactor: add_point and add_triangle to meshobject

--- zmesh/utility.hpp
+++ zmesh/utility.hpp
@@ -1,16 +1,11 @@
 #ifndef __ZMESH_UTILITY_HPP__
 #define __ZMESH_UTILITY_HPP__
 
+#include <algorithm>
 #include <vector>
 #include <limits>
 #include <cmath>
 #include <cstdint>
-
-struct MeshObject {
-  std::vector<float> points;
-  std::vector<float> normals;
-  std::vector<unsigned int> faces;
-};
 
 template <typename T = float>
 class Vec3 {
@@ -74,7 +69,21 @@ public:
   bool operator==(const Vec3& other) const {
     return x == other.x && y == other.y && z == other.z;
   }
-  T operator[](const int idx) const {
+  T& operator[](const int idx) {
+    if (idx == 0) {
+      return x;
+    }
+    else if (idx == 1) {
+      return y;
+    }
+    else if (idx == 2) {
+      return z;
+    }
+    else {
+      throw new std::runtime_error("Index out of bounds.");
+    }
+  }
+  T get(const int idx) const {
     if (idx == 0) {
       return x;
     }
@@ -128,6 +137,9 @@ public:
   int num_zero_dims() const {
     return (x == 0) + (y == 0) + (z == 0);
   }
+  int num_non_zero_dims() const {
+    return (x != 0) + (y != 0) + (z != 0);
+  }
   bool is_axis_aligned() const {
     return ((x != 0) + (y != 0) + (z != 0)) == 1;
   }
@@ -141,14 +153,264 @@ public:
   }
 };
 
+struct MeshObject {
+  std::vector<float> points;
+  std::vector<float> normals;
+  std::vector<unsigned int> faces;
+
+  void add_point(const Vec3<float>& pt) {
+    points.push_back(pt.x);
+    points.push_back(pt.y);
+    points.push_back(pt.z);
+  }
+
+  void add_triangle(
+    const unsigned int f1, 
+    const unsigned int f2, 
+    const unsigned int f3
+  ) {
+    faces.push_back(f1);
+    faces.push_back(f2);
+    faces.push_back(f3);
+  }
+
+  void add_triangle(const Vec3<unsigned int>& face) {
+    faces.push_back(face.x);
+    faces.push_back(face.y);
+    faces.push_back(face.z);
+  }
+
+  unsigned int last_face() const {
+    return (points.size() - 1) / 3;
+  }
+};
+
+Vec3<int32_t> zone2grid(int32_t zone, const Vec3<int32_t>& gs) {
+    int32_t z = zone / gs.x / gs.y;
+    int32_t y = (zone - gs.x * gs.y * z) / gs.x;
+    int32_t x = zone - gs.x * (y + gs.y * z);
+    return Vec3<int32_t>(x,y,z);
+}
+
+void fix_all_different(
+  const float* vertices,
+  const Vec3<float> minpt,
+  const std::vector<uint32_t>& face_remap,
+  const std::vector<int32_t>& zones,
+  std::vector<MeshObject>& mesh_grid,
+  const Vec3<float>& cs,
+  const Vec3<int32_t>& gs,
+  unsigned int f1, // f1 and f2 should match on zone
+  unsigned int f2,
+  unsigned int f3 // outlier
+) {
+
+  Vec3 v1(vertices[3*f1+0], vertices[3*f1+1], vertices[3*f1+2]);
+  Vec3 v2(vertices[3*f2+0], vertices[3*f2+1], vertices[3*f2+2]);
+  Vec3 v3(vertices[3*f3+0], vertices[3*f3+1], vertices[3*f3+2]);
+
+  auto z1 = zones[f1];
+  auto z2 = zones[f2];
+  auto z3 = zones[f3];
+
+  Vec3<int32_t> g1 = zone2grid(z1, gs);
+  Vec3<int32_t> g2 = zone2grid(z2, gs);
+  Vec3<int32_t> g3 = zone2grid(z3, gs);
+
+  Vec3<int32_t> delta12 = (g2 - g1).abs();
+  Vec3<int32_t> delta13 = (g3 - g1).abs();
+  Vec3<int32_t> delta23 = (g2 - g3).abs();
+
+  if (delta12.max() > 1 || delta13.max() > 1 || delta23.max() > 1) {
+    throw std::runtime_error("This code only handles differences of a single 26-connected grid space.");
+  }
+
+  if (delta12.num_non_zero_dims() == 3 
+    || delta13.num_non_zero_dims() == 3 
+    || delta23.num_non_zero_dims() == 3) {
+
+    throw std::runtime_error("3d all different not supported yet.");
+  }
+
+
+  // rearrange vertices so that vertex 1 is in the single 
+  // zone difference position for both 2 and 3
+  if (delta12.num_non_zero_dims() == 1 && delta13.num_non_zero_dims() != 1) {
+    std::swap(v1, v2);
+    std::swap(f1, f2);
+    std::swap(g1, g2);
+    std::swap(z1, z2);
+  }
+  else if (delta12.num_non_zero_dims() != 1 && delta13.num_non_zero_dims() == 1) {
+    std::swap(v1, v3);
+    std::swap(f1, f3);
+    std::swap(g1, g3);
+    std::swap(z1, z3);
+  }
+
+  delta12 = (g2 - g1);
+
+  int axis12 = 0;
+  if (delta12.y != 0) {
+    axis12 = 1;
+  }
+  else if (delta12.z != 0) {
+    axis12 = 2;
+  }
+
+  delta13 = (g3 - g1);
+
+  int axis13 = 0;
+  if (delta12.y != 0) {
+    axis13 = 1;
+  }
+  else if (delta12.z != 0) {
+    axis13 = 2;
+  }
+
+  float plane_offset12 = minpt.get(axis12) + std::max(g1[axis12], g2[axis12]) * cs.get(axis12);
+  float plane_offset13 = minpt.get(axis13) + std::max(g1[axis13], g2[axis13]) * cs.get(axis13);
+
+  auto intersect_fn = [](int axis, float plane_offset, const Vec3<float> &p, const Vec3<float> &q) {
+    float t = (plane_offset - p.get(axis)) / (p.get(axis) - q.get(axis));
+    return p + (p - q) * t;
+  };
+
+  const Vec3 i23_0 = intersect_fn(axis12, plane_offset12, v2, v3);
+  const Vec3 i23_1 = intersect_fn(axis13, plane_offset13, v2, v3);
+
+  Vec3 corner = i23_0;
+  corner[axis12] = plane_offset12;
+  corner[axis13] = plane_offset13;
+
+  MeshObject& m1 = mesh_grid[z1];
+  MeshObject& m2 = mesh_grid[z2];
+  MeshObject& m3 = mesh_grid[z3];
+  // MeshObject& m4 = mesh_grid[...];
+
+
+  const Vec3 i13 = intersect_fn(axis13, plane_offset13, v1, v3);
+  const Vec3 i12 = intersect_fn(axis12, plane_offset12, v1, v2);
+
+  unsigned int m3i13 = 0;
+  unsigned int m3i23_0 = 0;
+  unsigned int m3i23_1 = 0;
+  unsigned int m3corner = 0;
+
+  unsigned int m1i12 = 0;
+  unsigned int m1i13 = 0;
+  unsigned int m1i23_0= 0;
+  unsigned int m1i23_1 = 0;
+  unsigned int m1corner = 0;
+
+  unsigned int m2i12 = 0;
+  unsigned int m2i23_0 = 0;
+  unsigned int m2i23_1 = 0;
+  unsigned int m2corner = 0;
+
+  if (i23_0.get(axis12) <= plane_offset13) {
+    // 5 triangle situation
+
+    m3.add_point(i13);
+    m3i13 = m3.last_face();
+    m3.add_point(i23_1);
+    m3i23_1 = m3.last_face();
+    m3.add_triangle(face_remap[f3], m3i13, m3i23_1);
+
+    m1.add_point(i13);
+    m1i13 = m1.last_face();
+    m1.add_point(i23_1);
+    m1i23_1 = m1.last_face();
+    m1.add_point(i23_0);
+    m1i23_0 = m1.last_face();
+    m1.add_point(i12);
+    m1i12 = m1.last_face();
+
+    m1.add_triangle(face_remap[f1], m1i13, m1i23_1);
+    m1.add_triangle(face_remap[f1], m1i23_1, m1i23_0);
+    m1.add_triangle(face_remap[f1], m1i23_0, m1i12);
+
+    m2.add_point(i23_0);
+    m2i23_0 = m2.last_face();
+    m2.add_point(i12);
+    m2i12 = m2.last_face();
+
+    m2.add_triangle(face_remap[f2], m2i23_0, m2i12);
+  }
+  else {
+    m3.add_point(corner);
+    m3corner = m3.last_face();
+    m3.add_point(i13);
+    m3i13 = m3.last_face();
+    m3.add_point(i23_0);
+    m3i23_0 = m3.last_face();
+
+    m3.add_triangle(face_remap[f3], m3corner, m3i23_0);
+    m3.add_triangle(face_remap[f3], m3corner, m3i13);
+
+    // m4 single triangle
+
+    m1.add_point(i13);
+    m1i13 = m1.last_face();
+    m1.add_point(corner);
+    m1corner = m1.last_face();
+    m1.add_point(i12);
+    m1i12 = m1.last_face();
+
+    m1.add_triangle(face_remap[f1], m1i13, m1corner);
+    m1.add_triangle(face_remap[f1], m1corner, m1i12);
+
+    m2.add_point(i23_1);
+    m2i23_1 = m2.last_face();
+    m2.add_point(corner);
+    m2corner = m2.last_face();
+    m2.add_point(i12);
+    m2i12 = m2.last_face();
+
+    m2.add_triangle(face_remap[f2], m2corner, m2i23_1);
+    m2.add_triangle(face_remap[f2], m2corner, m2i12);
+  }
+}
+
+void fix_single_outlier_26_connected(
+  const float* vertices,
+  const Vec3<float> minpt,
+  const std::vector<uint32_t>& face_remap,
+  const std::vector<int32_t>& zones,
+  std::vector<MeshObject>& mesh_grid,
+  const Vec3<float>& cs,
+  const Vec3<int32_t>& gs,
+  const unsigned int f1, // f1 and f2 should match on zone
+  const unsigned int f2,
+  const unsigned int f3 // outlier
+) {
+
+  printf("single outlier 26 connected not implemented!\n");
+}
+
+void fix_single_outlier_18_connected(
+  const float* vertices,
+  const Vec3<float> minpt,
+  const std::vector<uint32_t>& face_remap,
+  const std::vector<int32_t>& zones,
+  std::vector<MeshObject>& mesh_grid,
+  const Vec3<float>& cs,
+  const Vec3<int32_t>& gs,
+  const unsigned int f1, // f1 and f2 should match on zone
+  const unsigned int f2,
+  const unsigned int f3 // outlier
+) {
+  printf("single outlier 18 connected not implemented!\n");
+}
+
 void fix_single_outlier_6_connected(
   const float* vertices,
   const Vec3<float> minpt,
   const std::vector<uint32_t>& face_remap,
-  const std::vector<uint32_t>& zones,
+  const std::vector<int32_t>& zones,
   std::vector<MeshObject>& mesh_grid,
   const Vec3<float>& cs,
-  const Vec3<uint32_t>& gs,
+  const Vec3<int32_t>& gs,
   const unsigned int f1, // f1 and f2 should match on zone
   const unsigned int f2,
   const unsigned int f3 // outlier
@@ -161,17 +423,14 @@ void fix_single_outlier_6_connected(
   auto z1 = zones[f1];
   auto z3 = zones[f3];
 
-  auto zone2grid = [&](uint32_t zone) {
-    uint32_t z = zone / gs.x / gs.y;
-    uint32_t y = (zone - gs.x * gs.y * z) / gs.x;
-    uint32_t x = zone - gs.x * (y + gs.y * z);
-    return Vec3<uint32_t>(x,y,z);
-  }; 
+  Vec3<int32_t> g1 = zone2grid(z1, gs);
+  Vec3<int32_t> g3 = zone2grid(z3, gs);
 
-  Vec3<uint32_t> g1 = zone2grid(z1);
-  Vec3<uint32_t> g3 = zone2grid(z3);
+  Vec3<int32_t> delta = g3 - g1;
 
-  auto delta = g3 - g1;
+  if (delta.abs().max() > 1) {
+    throw std::runtime_error("This code only handles differences of a single grid space.");
+  } 
 
   int axis = 0;
   if (delta.y != 0) {
@@ -181,54 +440,86 @@ void fix_single_outlier_6_connected(
     axis = 2;
   }
 
-  const float plane_offset = minpt[axis] + std::max(g1[axis], g3[axis]) * cs[axis];
+  const float plane_offset = minpt.get(axis) + std::max(g1[axis], g3[axis]) * cs.get(axis);
 
-  auto intersect_x_fn = [&](const Vec3<float> &p, const Vec3<float> &q) {
-    float t = (plane_offset - p[axis]) / (p[axis] - q[axis]);
+  auto intersect_fn = [&](const Vec3<float> &p, const Vec3<float> &q) {
+    float t = (plane_offset - p.get(axis)) / (p.get(axis) - q.get(axis));
     return p + (p - q) * t;
   };
 
-  const Vec3 i13 = intersect_x_fn(v1, v3);
-  const Vec3 i23 = intersect_x_fn(v2, v3);
+  const Vec3 i13 = intersect_fn(v1, v3);
+  const Vec3 i23 = intersect_fn(v2, v3);
 
   MeshObject& m1 = mesh_grid[z1];
   MeshObject& m3 = mesh_grid[z3];
 
-  m1.points.push_back(i13.x);
-  m1.points.push_back(i13.y);
-  m1.points.push_back(i13.z);
+  m1.add_point(i13);
 
-  const unsigned int m1f13 = (m1.points.size() - 1) / 3;
+  const unsigned int m1f13 = m1.last_face();
 
-  m1.points.push_back(i23.x);
-  m1.points.push_back(i23.y);
-  m1.points.push_back(i23.z);
+  m1.add_point(i23);
 
-  const unsigned int m1f23 = (m1.points.size() - 1) / 3;
+  const unsigned int m1f23 = m1.last_face();
 
-  m1.faces.push_back(face_remap[f1]);
-  m1.faces.push_back(m1f13);
-  m1.faces.push_back(m1f23);
+  m1.add_triangle(face_remap[f1], m1f13, m1f23);
+  m1.add_triangle(face_remap[f1], face_remap[f2], m1f23);
 
-  m1.faces.push_back(face_remap[f1]);
-  m1.faces.push_back(face_remap[f2]);
-  m1.faces.push_back(m1f23);
+  m3.add_point(i13);
+  
+  const unsigned int m3f13 = m3.last_face();
 
-  m3.points.push_back(i13.x);
-  m3.points.push_back(i13.y);
-  m3.points.push_back(i13.z);
+  m3.add_point(i23);
 
-  const unsigned int m3f13 = (m3.points.size() - 1) / 3;
+  const unsigned int m3f23 = m3.last_face();
 
-  m3.points.push_back(i23.x);
-  m3.points.push_back(i23.y);
-  m3.points.push_back(i23.z);
+  m3.add_triangle(face_remap[f3], m3f13, m3f23);
+}
 
-  const unsigned int m3f23 = (m3.points.size() - 1) / 3;
+void fix_single_outlier(
+  const float* vertices,
+  const Vec3<float> minpt,
+  const std::vector<uint32_t>& face_remap,
+  const std::vector<int32_t>& zones,
+  std::vector<MeshObject>& mesh_grid,
+  const Vec3<float>& cs,
+  const Vec3<int32_t>& gs,
+  const unsigned int f1, // f1 and f2 should match on zone
+  const unsigned int f2,
+  const unsigned int f3 // outlier
+) {
 
-  m3.faces.push_back(face_remap[f3]);
-  m3.faces.push_back(m3f13);
-  m3.faces.push_back(m3f23);
+  auto z1 = zones[f1];
+  auto z3 = zones[f3];
+
+  Vec3<int32_t> g1 = zone2grid(z1, gs);
+  Vec3<int32_t> g3 = zone2grid(z3, gs);
+
+  Vec3<int32_t> delta = g3 - g1;
+
+  if (delta.num_non_zero_dims() == 1) {
+    fix_single_outlier_6_connected(
+      vertices, minpt, 
+      face_remap, zones, 
+      mesh_grid, cs, gs,
+      f1, f2, f3
+    );
+  }
+  else if (delta.num_non_zero_dims() == 2) {
+    fix_single_outlier_18_connected(
+      vertices, minpt, 
+      face_remap, zones, 
+      mesh_grid, cs, gs,
+      f1, f2, f3
+    );
+  }
+  else if (delta.num_non_zero_dims() == 3) {
+    fix_single_outlier_26_connected(
+      vertices, minpt, 
+      face_remap, zones, 
+      mesh_grid, cs, gs,
+      f1, f2, f3
+    );
+  }
 }
 
 std::vector<MeshObject> chunk_mesh_accelerated(
@@ -261,13 +552,13 @@ std::vector<MeshObject> chunk_mesh_accelerated(
 
   const Vec3 minpt(min_x, min_y, min_z);
 
-  const uint32_t gx = std::max(static_cast<uint32_t>(((max_x - min_x) / cx) + 0.5), static_cast<uint32_t>(1));
-  const uint32_t gy = std::max(static_cast<uint32_t>(((max_y - min_y) / cy) + 0.5), static_cast<uint32_t>(1));
-  const uint32_t gz = std::max(static_cast<uint32_t>(((max_z - min_z) / cz) + 0.5), static_cast<uint32_t>(1));
+  const int32_t gx = std::max(static_cast<int32_t>(((max_x - min_x) / cx) + 0.5), static_cast<int32_t>(1));
+  const int32_t gy = std::max(static_cast<int32_t>(((max_y - min_y) / cy) + 0.5), static_cast<int32_t>(1));
+  const int32_t gz = std::max(static_cast<int32_t>(((max_z - min_z) / cz) + 0.5), static_cast<int32_t>(1));
 
-  const Vec3<uint32_t> gs(gx,gy,gz);
+  const Vec3<int32_t> gs(gx,gy,gz);
 
-  std::vector<uint32_t> zones(num_vertices);
+  std::vector<int32_t> zones(num_vertices);
 
   const float epsilon = 1e-4;
 
@@ -310,7 +601,7 @@ std::vector<MeshObject> chunk_mesh_accelerated(
 
     if (!(zones[f1] == zones[f2] && zones[f1] == zones[f3])) {
       if (zones[f1] == zones[f2]) {
-        fix_single_outlier_6_connected(
+        fix_single_outlier(
           vertices, minpt, 
           face_remap, zones, 
           mesh_grid, cs, gs,
@@ -318,7 +609,7 @@ std::vector<MeshObject> chunk_mesh_accelerated(
         );
       }
       else if (zones[f1] == zones[f3]) {
-        fix_single_outlier_6_connected(
+        fix_single_outlier(
           vertices, minpt, 
           face_remap, zones, 
           mesh_grid, cs, gs,
@@ -326,16 +617,21 @@ std::vector<MeshObject> chunk_mesh_accelerated(
         );
       }
       else if (zones[f2] == zones[f3]) {
-        fix_single_outlier_6_connected(
+        fix_single_outlier(
           vertices, minpt, 
           face_remap, zones, 
           mesh_grid, cs, gs,
           f2, f3, f1
         );
       }
-      // else {
-      //   // do nothing
-      // }
+      else {
+        // fix_all_different(
+        //   vertices, minpt, 
+        //   face_remap, zones, 
+        //   mesh_grid, cs, gs,
+        //   f1, f2, f3
+        // );
+      }
 
       continue;
     }
