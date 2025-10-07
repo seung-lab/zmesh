ec48f5e36e36f55b33665ff1369d562c86fc9b31
William Silversmith <william.silversmith@gmail.com>
2025-10-07 18:35:13 -0400

    build: add mac build script

---
+++ buildmac.zsh
@@ -0,0 +1,31 @@
+rm -r build *.egg-info
+
+export VERSIONS="9 10 11 12 13 14"
+
+for ver in $VERSIONS; do
+	rm -r "~/.virtualenvs/zmesh3$ver"
+	mkvirtualenv -p "python3.$ver" "zmesh3$ver"
+	pip install -r requirements.txt -r dev_requirements.txt
+	pip install pbr setuptools wheel build twine
+done
+
+workon zmesh39
+python setup.py sdist
+
+for plat in x86_64; do
+	export ARCHFLAGS="-arch $plat"
+	if [ "$plat" = "arm64" ]; then
+		export PLATNAME=macosx_11_0_arm64
+	else
+		export PLATNAME=macosx_10_9_x86_64
+	fi
+
+	for ver in $VERSIONS; do
+		workon "zmesh3$ver"
+		# if [ "$plat" = "arm64" ]; then
+		# 	python setup.py develop
+		# 	python -m pytest -v automated_test.py
+		# fi
+		python -m build --wheel --config-setting=--build-option="--plat-name=$PLATNAME"
+	done
+done
