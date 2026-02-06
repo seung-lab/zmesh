010c4b31846450f090a5745bfb5d0c64e2832826
William Silversmith <william.silversmith@gmail.com>
2026-02-05 22:10:07 -0500

    build: more robust mac build script

--- buildmac.zsh
+++ buildmac.zsh
@@ -1,18 +1,19 @@
 rm -r build *.egg-info
 
-export VERSIONS="9 10 11 12 13 14"
+export VERSIONS=(9 10 11 12 13 14)
 
 for ver in $VERSIONS; do
+	echo "$ver"
 	rm -r "~/.virtualenvs/zmesh3$ver"
 	mkvirtualenv -p "python3.$ver" "zmesh3$ver"
-	pip install -r requirements.txt -r dev_requirements.txt
-	pip install pbr setuptools wheel build twine
+	uv pip install -r requirements.txt -r dev_requirements.txt
+	uv pip install pbr setuptools wheel build twine
 done
 
 workon zmesh39
 python setup.py sdist
 
-for plat in x86_64; do
+for plat in arm64 x86_64; do
 	export ARCHFLAGS="-arch $plat"
 	if [ "$plat" = "arm64" ]; then
 		export PLATNAME=macosx_11_0_arm64
